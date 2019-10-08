04560521df808d123fd5bd241f9526e5401ca0c6
valeffe1109 <valeriofusco1109v@gmail.com>
2019-10-08 15:52:58 +0200

    update

--- services/students.js
+++ services/students.js
@@ -7,44 +7,44 @@ const router = express.Router()
 
 
 router.get("/", async (req, res) => {
-    var selectStudents = "SELECT * FROM STUDENTS"
-    if (req.query.Name)
-      selectStudents += " WHERE Name = '" + req.query.Name + "'";
+  var selectStudents = "SELECT * FROM STUDENTS"
+  if (req.query.Name)
+    selectStudents += " WHERE Name = '" + req.query.Name + "'";
 
-      console.log(selectStudents)
+  console.log(selectStudents)
 
-    var request = new Request(selectStudents, (err, rowCount, rows) =>{
-      if(err) res.send(err)
-      else res.send(Students)
+  var request = new Request(selectStudents, (err, rowCount, rows) => {
+    if (err) res.send(err)
+    else res.send(Students)
+  })
+
+  var Students = [];
+  request.on('row', (columns) => { //every time we receive back a row from SQLServer
+    var Student = {}
+    columns.forEach(column => {
+      Student[column.metadata.colName.toLowerCase()] = column.value //add property to the Student object
     })
-
-    var Students = [];
-    request.on('row', (columns) => { //every time we receive back a row from SQLServer
-      var Student = {}
-      columns.forEach(column =>{
-        Student[column.metadata.colName.toLowerCase()] = column.value //add property to the Student object
-      })
-      Students.push(Student);
-    })
-    connection.execSql(request); //Execute Query
+    Students.push(Student);
+  })
+  connection.execSql(request); //Execute Query
 })
 
 router.get("/:id", async (req, res) => {
   var selectStudents = "SELECT * FROM STUDENTS WHERE StudentsID = @StudentsID"
-  var request = new Request(selectStudents, (err, rowCount, rows) =>{
-    if(err) res.send(err)
+  var request = new Request(selectStudents, (err, rowCount, rows) => {
+    if (err) res.send(err)
     else {
-        if (rowCount == 1)
-           res.send(student)
-        else
-              res.status(404).send("Cannot find element " + req.params.id)
+      if (rowCount == 1)
+        res.send(student)
+      else
+        res.status(404).send("Cannot find element " + req.params.id)
     }
   })
 
-  var student= {};
+  var student = {};
   request.on('row', (columns) => {
-    columns.forEach(column =>{
-      student[column.metadata.colName.toLowerCase()] = column.value 
+    columns.forEach(column => {
+      student[column.metadata.colName.toLowerCase()] = column.value
     })
   })
   request.addParameter("StudentsID", Types.NVarChar, req.params.id)
@@ -52,16 +52,37 @@ router.get("/:id", async (req, res) => {
   connection.execSql(request); //Execute Query
 })
 
+router.get('/:id/Projects', async (req, res) => {
+  var selectProject = 'SELECT * FROM PROJECTS WHERE StudentsID = @StudentsID'
+  var request = new Request(selectProject, (err, rowCount, rows) => {
+    if (err) res.send(err)
+    else {
+      if (rowCount == 1)
+        res.send(project)
+      else
+        res.status(404).send('Cannot find element' + req.params.id)
+    }
+  })
+  var project = {};
+  request.on('row', (columns) => {
+    columns.forEach(column => {
+      project[column.metadata.colName.toLowerCase()] = column.value
+    })
+  })
+  request.addParameter('StudentsID', Tupes.NVarChar, req.params.id)
+  connection.execSql(request);
+})
+
 
 router.post("/", async (req, res) => {
-    var selectStudents= `INSERT INTO STUDENTS (Name, Surname, Email)
+  var selectStudents = `INSERT INTO STUDENTS (Name, Surname, Email)
                        VALUES ('${req.body.Name}', '${req.body.Surname}', '${req.body.Email}')`
 
-    var request = new Request(selectStudents, (err) =>{ 
-        if(err) res.send(err)
-        else res.send("Student Added")
-     })
-    connection.execSql(request); //Execute Query
+  var request = new Request(selectStudents, (err) => {
+    if (err) res.send(err)
+    else res.send("Student Added")
+  })
+  connection.execSql(request); //Execute Query
 })
 
 router.post("/:id/addProject", (req, res) => {
@@ -69,7 +90,7 @@ router.post("/:id/addProject", (req, res) => {
                    OUTPUT Inserted.ProjectID
                    VALUES ('${req.body.TITLE}','${req.body.GITLINK}','${req.body.LIVEPROJECT}','${req.params.id}')`
 
-  var request = new Request(addProject, (err, rowCount, rows) =>{
+  var request = new Request(addProject, (err, rowCount, rows) => {
     if (err) res.send(err)
     else res.send('project added')
   })
